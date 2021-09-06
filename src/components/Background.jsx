f7c7d5f57939b7e9ca685d4af3af011b450b2799
Pablo Gimenez <pablo.gimenez@mediamonks.com>
2021-09-06 14:57:06 -0300

    Center child component

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,8 +3,14 @@ import { Box, Flex, Spacer, Avatar, Heading } from '@chakra-ui/react'
 
 export default function Background({ children }) {
   return (
-    <Flex height="100%" minHeight="100vh" direction="column">
-      <Box padding="2em">
+    <Flex
+      height="100%"
+      minHeight="100vh"
+      padding="2em"
+      justifyContent="space-between"
+      flexDirection="column"
+    >
+      <Box width="100%" height="100%">
         <Flex mb="8" alignItems="center" justifyContent="space-between">
           <Box>
             <Heading fontSize="32pt" fontWeight="medium" color="white">
@@ -16,8 +22,11 @@ export default function Background({ children }) {
             <Avatar name="A" bg="rgba(255, 255, 255, 0.1)" />
           </Box>
         </Flex>
+      </Box>
+      <Box width="100%" height="100%">
         {children}
       </Box>
+      <Box />
     </Flex>
   )
 }
