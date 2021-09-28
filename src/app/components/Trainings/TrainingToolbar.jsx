1ee6fdfdd369f1edc69ba27c07572671cbbb4728
ana.laclau <ana.laclau@mediamonks.com>
2021-09-28 18:01:56 -0300

    training list layout done

--- src/app/components/Trainings/TrainingToolbar.jsx
+++ src/app/components/Trainings/TrainingToolbar.jsx
@@ -1,26 +1,46 @@
-import { Button, Icon, Grid } from '@chakra-ui/react'
+import { Button, Icon, Box } from '@chakra-ui/react'
 import { IoPlay } from 'react-icons/io5'
 import { EditButton } from '../shared/Buttons'
+import { DeleteIcon } from '@chakra-ui/icons'
 
-export const TrainingToolbar = ({ startTraining, editTraining }) => {
+export const TrainingToolbar = ({ startTraining, editTraining, deleteTraining }) => {
   return (
     <>
-      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+      <Box>
         <EditButton
-          size="sm"
+          margin="5px"
+          width="100px"
+          border="1px solid #FFFFFF"
+          borderRadius="500px"
+          size="xs"
+          onClick={deleteTraining}
+          leftIcon={<Icon as={DeleteIcon} />}
+        >
+          Delete
+        </EditButton>
+        <EditButton
+          margin="5px"
+          width="100px"
+          border="1px solid #FFFFFF"
+          borderRadius="500px"
+          size="xs"
           onClick={editTraining}
         >
           Edit
         </EditButton>
         <Button
+          margin="5px"
+          width="100px"
+          border="1px solid #FFFFFF"
+          borderRadius="500px"
           onClick={startTraining}
           variant="primary-trueblue"
-          size="sm"
+          size="xs"
           leftIcon={<Icon as={IoPlay} />}
         >
           Start
         </Button>
-      </Grid>
+      </Box>
     </>
   )
 }
