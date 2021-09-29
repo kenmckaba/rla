6878f92b0f737c18c7ccdb5f6442e7bf0d89a005
Gonzalo Spota <gonzalo.spota@mediamonks.com>
2021-09-29 15:46:02 -0300

    General cleanup + Mic and Camera toggle controls

--- src/app/pages/PreviewScreen/PreviewScreen.jsx
+++ src/app/pages/PreviewScreen/PreviewScreen.jsx
@@ -1,64 +1,19 @@
-import React, { useEffect, useMemo, useRef } from 'react'
-import { getTraining } from '../../../graphql/queries'
-import { gql, useQuery, useMutation } from '@apollo/client'
+//import React, { useEffect, useMemo, useRef } from 'react'
 import {
   Box,
-  VStack,
   Button,
   Modal,
   ModalOverlay,
   ModalContent,
   ModalHeader,
   ModalBody,
-  useDisclosure,
-  Flex,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Tr,
-  Td,
-  Thead,
-  Th,
-  Table,
-  Tbody,
-  Heading,
-  Text,
-  Spacer,
-  IconButton,
   Icon,
-  HStack,
-  Center,
-  //mio
+  Flex,
   Container,
 } from '@chakra-ui/react'
 import { EditButton, PrimaryButton } from '../../components/shared/Buttons'
-import { updateTraining } from '../../../graphql/mutations'
 import { useState } from 'react'
-import {
-  onCreateAttendee,
-  onCreatePoll,
-  onDeleteAttendee,
-  onDeletePoll,
-  onUpdateAttendee,
-  onUpdatePoll,
-} from '../../../graphql/subscriptions'
-import { buildSubscription } from 'aws-appsync'
-import { TrainerPoll } from '../../components/TrainerPoll'
-import { PollModal } from '../../components/PollModal'
-import { useBlueJeans } from '../../../bluejeans/useBlueJeans'
-import { ClassRoster } from '../../components/ClassRoster'
-import { AddIcon } from '@chakra-ui/icons'
-import { MicCamControls } from '../../components/MicCamControls'
-import { BjnMedia } from '../../components/BjnMedia'
-import { CamInUseModal } from '../../components/CamInUseModal'
 import { FaCamera, FaMicrophone, FaVideo } from 'react-icons/fa'
-import { prettyTime } from '../../../pretty-time'
-import MiddlePanel from '../../components/TrainerInSession/MiddlePanel'
-import RightPanel from '../../components/TrainerInSession/RightPanel'
-
-// MIOO
 import { H1Heading, H3Heading } from '../../components/shared/Heading'
 import './PreviewScreen.css'
 
@@ -67,160 +22,14 @@ export const PreviewScreen = ({
     params: { trainingId },
   },
 }) => {
-  const [training, setTraining] = useState()
-  const [attendees, setAttendees] = useState([])
-  const [pollToEdit, setPollToEdit] = useState()
-  const [polls, setPolls] = useState([])
-  const [startedPoll, setStartedPoll] = useState()
-  const [startTimeUpdated, setStartTimeUpdated] = useState(false)
-  const joined = useRef(false)
-  const localVideoRef = useRef(null)
-  const gotVideosRef = useRef(false)
-  const { bjnApi, bjnIsInitialized, bjnCamInUseError } = useBlueJeans()
-  const {
-    data: trainingData,
-    error,
-    loading,
-    subscribeToMore,
-  } = useQuery(gql(getTraining), {
-    variables: { id: trainingId },
-  })
-  const [updateCurrentTraining, { error: updateError }] = useMutation(gql(updateTraining))
-  const { isOpen: isEndModalOpen, onOpen: onEndModalOpen } = useDisclosure()
-  const {
-    isOpen: isPollModalOpen,
-    onOpen: onPollModalOpen,
-    onClose: onPollModalClose,
-  } = useDisclosure()
-
-  const addAPoll = () => {
-    setPollToEdit(null)
-    onPollModalOpen()
+  const [toggleControlCam, setToggleControlCam] = useState(false)
+  const toggleControlButtonCam = () => {
+    setToggleControlCam(!toggleControlCam)
   }
-
-  const Polls = useMemo(() => {
-    const startPoll = (poll) => {
-      updateCurrentTraining({
-        variables: {
-          input: {
-            id: training.id,
-            currentPollId: poll ? poll.id : '',
-            pollMode: poll?.stoppedAt ? 'SHOWRESULTS' : 'POLL',
-          },
-        },
-      })
-    }
-
-    const editPoll = (p) => {
-      setPollToEdit(p)
-      onPollModalOpen()
-    }
-
-    if (polls.length === 0) {
-      return <Box>*None*</Box>
-    }
-    return polls.map((poll) => {
-      return (
-        <TrainerPoll
-          key={poll.id}
-          pollId={poll.id}
-          startPoll={startPoll}
-          startedPoll={startedPoll}
-          editPoll={() => editPoll(poll)}
-        />
-      )
-    })
-  }, [polls, startedPoll, updateCurrentTraining, training?.id, onPollModalOpen])
-
-  useEffect(() => {
-    if (subscribeToMore) {
-      const cleanupFuncs = [
-        subscribeToMore(buildSubscription(gql(onCreateAttendee), gql(getTraining))),
-        subscribeToMore(buildSubscription(gql(onUpdateAttendee), gql(getTraining))),
-        subscribeToMore(buildSubscription(gql(onDeleteAttendee), gql(getTraining))),
-        subscribeToMore(buildSubscription(gql(onUpdatePoll), gql(getTraining))),
-        subscribeToMore(buildSubscription(gql(onCreatePoll), gql(getTraining))),
-        subscribeToMore(buildSubscription(gql(onDeletePoll), gql(getTraining))),
-      ]
-      /*       return () => {
-        cleanupFuncs.forEach((func) => func())
-      } */
-    }
-  }, [subscribeToMore])
-
-  useEffect(() => {
-    if (trainingData && (!training || trainingId === trainingData?.getTraining?.id)) {
-      const tr = trainingData.getTraining
-      setTraining(tr)
-      setAttendees(tr.attendees.items)
-
-      const polls = tr.polls.items
-      setPolls(polls)
-      if (tr.currentPollId) {
-        const started = polls.find((poll) => poll.id === tr.currentPollId)
-        setStartedPoll(started)
-      } else {
-        setStartedPoll(null)
-      }
-    }
-  }, [trainingData, trainingId, training])
-
-  useEffect(() => {
-    if (training && !startTimeUpdated) {
-      setStartTimeUpdated(true)
-      updateCurrentTraining({
-        variables: {
-          input: {
-            id: training.id,
-            startedAt: Date.now(),
-          },
-        },
-      })
-    }
-  }, [training, updateCurrentTraining, startTimeUpdated])
-
-  useEffect(() => {
-    const joinMeeting = async () => {
-      if (training && bjnIsInitialized && !joined.current) {
-        joined.current = true
-        try {
-          await bjnApi.join(training.meetingId, training.moderatorPasscode, training.trainerName)
-        } catch (e) {
-          console.error('rla-log: error joining', e)
-        }
-      }
-    }
-    joinMeeting()
-  }, [training, bjnApi, bjnIsInitialized])
-
-  useEffect(() => {
-    if (localVideoRef.current && !gotVideosRef.current) {
-      bjnApi.attachLocalVideo(localVideoRef.current)
-      gotVideosRef.current = true
-    }
-  })
-
-  const endTraining = () => {
-    bjnApi.leave(true)
-    onEndModalOpen()
-    updateCurrentTraining({
-      variables: {
-        input: {
-          id: trainingId,
-          endedAt: Date.now(),
-        },
-      },
-    })
+  const [toggleControlMic, setToggleControlMic] = useState(false)
+  const toggleControlButtonMic = () => {
+    setToggleControlMic(!toggleControlMic)
   }
-
-  if (error || updateError) {
-    return <p>An error occured</p>
-  }
-
-  if (loading || !training) {
-    return <p>Please wait...</p>
-  }
-
   return (
     <>
       <Container maxW="container.xl" maxWidth="unset">
@@ -242,20 +51,20 @@ export const PreviewScreen = ({
             <H1Heading style={{textAlign: 'center'}}>
               Hello Ken.<br></br>Let’s check out the camera and mic
             </H1Heading>
-            <div className="preview-screen">
+            <div className={`${toggleControlCam ? 'preview-screen toggle-screen' : 'preview-screen'}`}>
               <EditButton w="30%" h="12%" className="preview-settings">
                 Settings
               </EditButton>
               <div className="preview-controls">
-                <Icon as={FaVideo} className="preview-cam"/>
-                <Icon as={FaMicrophone} className="preview-mic"/>
+                <Icon as={FaVideo} onClick={() => toggleControlButtonCam(!toggleControlCam)} className={`${toggleControlCam ? 'preview-cam toggle-control-cam' : 'preview-cam'}`}/>
+                <Icon as={FaMicrophone} onClick={() => toggleControlButtonMic(!toggleControlMic)} className={`${toggleControlMic ? 'preview-mic toggle-control-mic' : 'preview-mic'}`}/>
               </div>
             </div>
             <Flex flexDirection="row" w="100%" alignItems="center" justifyContent="center">
-              <EditButton w="15%" onClick={endTraining}>
+              <EditButton w="15%">
                 Go Back
               </EditButton>
-              <PrimaryButton w="15%" onClick={endTraining} style={{marginLeft: '20px'}}>
+              <PrimaryButton w="15%" style={{marginLeft: '20px'}}>
                 Start training
               </PrimaryButton>
             </Flex>
