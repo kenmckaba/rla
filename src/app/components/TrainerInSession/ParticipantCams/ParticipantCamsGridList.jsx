53df2c0ad8d88bd3096fbab30aa02815dcd34520
andressuarezmonk <andres.suarez@mediamonks.com>
2021-10-01 08:33:26 -0300

    feat: add pagination badge

--- src/app/components/TrainerInSession/ParticipantCams/ParticipantCamsGridList.jsx
+++ src/app/components/TrainerInSession/ParticipantCams/ParticipantCamsGridList.jsx
@@ -1,5 +1,7 @@
 import ParticipantCam from './ParticipantCam'
-import { Flex } from '@chakra-ui/react'
+import { Badge, Box, Center, Flex, HStack, Text } from '@chakra-ui/react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
+import { IconButton } from '@chakra-ui/button'
 
 export default function ParticipantCamsGridList({flex, ...props}) {
 
@@ -19,9 +21,37 @@ export default function ParticipantCamsGridList({flex, ...props}) {
   ]
 
   return (
-    <Flex flexWrap="wrap" height="94vh" {...props}>
-      {attendeesList.map(attendee => <ParticipantCam picture={attendee.picture} name={attendee.name} flex={flex} maxWidth="100%" />)}
-    </Flex>
+    <Box position="relative">
+      <Flex flexWrap="wrap" height="94vh" {...props}>
+        {attendeesList.map(attendee => <ParticipantCam picture={attendee.picture} name={attendee.name} flex={flex} maxWidth="100%" />)}
+      </Flex>
+      <Badge
+        position="absolute"
+        right="0"
+        bottom="2"
+        width="100px"
+        height="48px"
+        color="white"
+        background="rgba(0,0,0,0.5)"
+        borderRadius="full"
+      >
+        <Center height="48px">
+          <HStack>
+            <IconButton
+              aria-label="Previous page of cams"
+              variant="unstyled"
+              icon={<ChevronLeftIcon />}
+            />
+            <Text>1/2</Text>
+            <IconButton
+              aria-label="Next page of cams"
+              variant="unstyled"
+              icon={<ChevronRightIcon />}
+            />
+          </HStack>
+        </Center>
+      </Badge>
+    </Box>
   )
 
 }
