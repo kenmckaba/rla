9ed210f90528961097d4d178225afdb24b11ade5
hectorMarrufo <hector.marrufo@mediamonks.com>
2021-12-21 11:38:51 -0300

    removed title on login page for non authorized users

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,12 +1,29 @@
 import { Flex } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Hub } from '@aws-amplify/core'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react'
+import { Auth } from 'aws-amplify'
 import { H3Heading } from './shared/Heading'
 const location = window.location.pathname
 
 export default function Background({ children }) {
+  const [currentUser, setCurrentUser] = useState({})
+  useEffect(() => {
+    let updateUser = async () => {
+      try {
+        let user = await Auth.currentAuthenticatedUser()
+        setCurrentUser(user)
+      } catch {
+        setCurrentUser(null)
+      }
+    }
+    Hub.listen('auth', updateUser) // listen for login/signup events
+    updateUser() // check manually the first time because we won't get a Hub event
+    return () => Hub.remove('auth', updateUser) // cleanup
+  }, [])
   return (
     <Flex minHeight="100vh" flexDirection="column">
-      {location === '/' && (
+      {location === '/' && currentUser && (
         <Flex
           alignItems="center"
           justifyContent="space-between"
