cbd71885a3ac1a7168154c165bb28e69c06ef9e8
Pooja Kanuri <pooja.kanuri@verizon.com>
2022-06-22 15:15:30 -0700

    updated aws keys

--- src/utils/ses-params.js
+++ src/utils/ses-params.js
@@ -1,5 +1,5 @@
 export const sesParams = {
   region: 'us-east-1',
-  accessKeyId: 'AKIATZDAET4G5MQCWIH6',
-  secretAccessKey: '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',
+  accessKeyId: 'AKIATZDAET4GSA2O6RGK', // AKIATZDAET4G5MQCWIH6
+  secretAccessKey: 'BMeReEzHv3qm7PzjcSSBtCdc+KVYnPAQwNTLgavSveBR', // ZA0P86xxsAlMsibzsJ8rwElN9ZSqgpGfHU4BuqRC
 }
