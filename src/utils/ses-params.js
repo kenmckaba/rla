25ba24f2dc53e1cbff7435ec927a67538e7bca90
Pooja Kanuri <pooja.kanuri@verizon.com>
2022-06-22 18:43:22 -0700

    updated access key

--- src/utils/ses-params.js
+++ src/utils/ses-params.js
@@ -1,5 +1,5 @@
 export const sesParams = {
   region: 'us-east-1',
-  accessKeyId: 'AKIATZDAET4GSA2O6RGK', // AKIATZDAET4G5MQCWIH6
-  secretAccessKey: 'BMeReEzHv3qm7PzjcSSBtCdc+KVYnPAQwNTLgavSveBR', // ZA0P86xxsAlMsibzsJ8rwElN9ZSqgpGfHU4BuqRC
+  accessKeyId: 'AKIATZDAET4G35BZTOK2', 
+  secretAccessKey: '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', 
 }
