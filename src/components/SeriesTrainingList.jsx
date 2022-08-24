c0d59508f31bd70ef12395a2893b571aa8063593
Pooja Kanuri <pooja.kanuri@verizon.com>
2022-08-24 11:28:13 -0700

    new training header

--- src/components/SeriesTrainingList.jsx
+++ src/components/SeriesTrainingList.jsx
@@ -194,8 +194,7 @@ export const SeriesTrainingList = ({ series, deleteTraining, saveSeries }) => {
         <ModalContent color="darkKnight.700">
           <ModalHeader>
             <Flex>
-              {/* <Box>{newTraining ? 'New Training' : 'Update Training'}</Box> */}
-              {/* <Box>{newTraining ? (currentTraining?.type === 'SERIES' ? 'New Series' : 'New Training') : (currentTraining?.type ==='SERIES' ? 'Update Series' : 'Update Training')}</Box> */}
+              <Box>{newTraining ? 'New Training' : 'Update Training'}</Box>
               <Spacer></Spacer>
               <Box>
                 <HStack spacing={2}>
