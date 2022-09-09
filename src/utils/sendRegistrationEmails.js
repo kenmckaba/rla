2f0ccd830487364dea75cb1abaa95d993a4d847e
Pooja Kanuri <pooja.kanuri@verizon.com>
2022-09-09 10:18:27 -0700

    series email changes

--- src/utils/sendRegistrationEmails.js
+++ src/utils/sendRegistrationEmails.js
@@ -52,7 +52,7 @@ export const sendRegistrationEmails = async (training, allAttendees) => {
     const params = {
       Destinations: templates,
       Source: 'ccer@umb.edu',
-      Template: 'RLAInviteToRegister',
+      Template: training.type === 'SERIES' ? 'SeriesInviteRegister' : 'RLAInviteToRegister',
       DefaultTemplateData,
     }
     try {
