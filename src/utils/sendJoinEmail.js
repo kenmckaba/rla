90d9cf7da247fd05146bff49bf5c57812c0c1d5d
Pooja Kanuri <pooja.kanuri@verizon.com>
2022-09-27 15:57:06 -0700

    working on using aws without sesparams

--- src/utils/sendJoinEmail.js
+++ src/utils/sendJoinEmail.js
@@ -1,11 +1,45 @@
+/* eslint-disable indent */
 import aws from 'aws-sdk'
 import { timestampToPrettyTime } from './pretty-time'
 
-import { sesParams } from './ses-params'
+// import { sesParams } from './ses-params'
 
-const ses = new aws.SES(sesParams)
+// const ses = new aws.SES(sesParams)
+
 
 export const sendJoinEmail = async (attendeeId, name, email, training) => {
+
+  // Load the AWS SDK for Node.js
+  var AWS = require('aws-sdk')
+  // Set the region 
+  AWS.config.update({region: 'us-east-1'}) // need to add region
+
+  // Create sendEmail params 
+// var params = {
+//   Destinations: [template],
+//   Message: { /* required */
+//     Body: { /* required */
+//       Html: {
+//        Charset: 'UTF-8',
+//        Data: 'HTML_FORMAT_BODY'
+//       },
+//       Text: {
+//        Charset: 'UTF-8',
+//        Data: 'TEXT_FORMAT_BODY'
+//       }
+//      },
+//      Subject: {
+//       Charset: 'UTF-8',
+//       Data: 'Test email'
+//      }
+//     },
+//   Source: 'ccer@umb.edu', /* required */
+//   // ReplyToAddresses: [
+//   //    'EMAIL_ADDRESS',
+//   //   /* more items */
+//   // ],
+// }
+
   const template = {
     Destination: {
       ToAddresses: [email],
@@ -39,12 +73,26 @@ export const sendJoinEmail = async (attendeeId, name, email, training) => {
     Template: 'RLAJoinTraining',
     DefaultTemplateData,
   }
-  console.log(JSON.stringify(params))
-  try {
-    // const result = await ses.sendBulkTemplatedEmail(params).promise()
-    const result = await ses.sendBulkTemplatedEmail(params).promise()
-    console.log('send join email result', result)
-  } catch (err) {
-    console.error('send join email result', err)
-  }
+  // console.log(JSON.stringify(params))
+  // try {
+  //   // const result = await ses.sendBulkTemplatedEmail(params).promise()
+  //   const result = await ses.sendBulkTemplatedEmail(params).promise()
+  //   console.log('send join email result', result)
+  // } catch (err) {
+  //   console.error('send join email result', err)
+  // }
+
+
+
+  // Create the promise and SES service object
+  var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendBulkTemplatedEmail(params).promise()
+
+  // Handle promise's fulfilled/rejected states
+  sendPromise.then(
+    function(data) {
+      console.log(data.MessageId)
+    }).catch(
+    function(err) {
+      console.error(err, err.stack)
+    })
 }
